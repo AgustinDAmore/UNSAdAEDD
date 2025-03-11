f7cb73fba127be2b7b153f8093d792659b316a98
Agustin <agustindamore50@gmail.com>
2025-03-11 13:15:04 -0300

    Se creo l EDD Cola

---
+++ Cola.py
@@ -0,0 +1,47 @@
+class Cola:
+    def __init__(self):
+        self.items = []
+        self.tamanio = 0
+    
+    def encolar(self,item):
+        self.items.insert(0,item)
+        self.tamanio+=1
+    
+    def desencolar(self):
+        if self.vacia():
+            print("No hay elementos en la Cola")
+        else:
+            self.items.pop()
+            self.tamanio-=1
+    
+    def vacia(self):
+        return self.tamanio==0
+    
+    def __repr__(self):
+        if self.vacia():
+            return "Cola: [] (Vacía)"
+        else:
+            elementos = ", ".join(map(str, self.items)) # Convierte los items a string y los une con comas
+            return f"Cola: [{elementos}] (Frente -> Atras)"
+
+    
+
+cola = Cola()
+
+cola.desencolar()
+
+print("Cant elementos en la Cola: ",cola.tamanio)
+print("Mostramos los elementos dentro de la cola: ",cola)
+
+cola.encolar(1)
+cola.encolar(2)
+cola.encolar(5)
+cola.encolar(4)
+
+print("Cant elementos en la Cola: ",cola.tamanio)
+print("Mostramos los elementos dentro de la cola: ",cola)
+
+cola.desencolar()
+
+print("Cant elementos en la Cola: ",cola.tamanio)
+print("Mostramos los elementos dentro de la cola: ",cola)
